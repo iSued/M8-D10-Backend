010edf3dee4f8178001db57cb14e8ddd17a743e4
iSued <59610863+iSued@users.noreply.github.com>
2021-02-28 22:08:05 +0100

    Update index.js

--- src/services/weatherApi/index.js
+++ src/services/weatherApi/index.js
@@ -4,11 +4,24 @@ const { authorize } = require("../auth/middlewares");
 
 const WeatherRouter = express.Router();
 
-WeatherRouter.get("/:city", authorize, async (req, res, next) => {
+WeatherRouter.get("/city/:city", authorize, async (req, res, next) => {
   try {
     await axios
       .get(
         `https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${process.env.WEATHER_API_KEY}`
+      )
+      .then((response) => res.send(response.data));
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+WeatherRouter.get("/coordinates", authorize, async (req, res, next) => {
+  try {
+    await axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${req.query.lat}&lon=${req.query.lon}&appid=${process.env.WEATHER_API_KEY}`
       )
       .then((response) => res.send(response.data));
   } catch (error) {
